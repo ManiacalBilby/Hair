879eb1b4664adb0ce5ff1656c52bd2d2722f9980
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-02-26 12:28:13 -0500

    Refactored all spec/models/FILENAME to organize validation tests to prep for testing associations

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -7,19 +7,22 @@ RSpec.describe Appointment, type: :model do
     start_date: "2018-03-02",
     duration: 120)
   }
-  it "is valid with valid attributes" do
-    expect(subject).to be_valid
-  end
-  it "is not valid without a start time" do
-    subject.start_time = nil
-    expect(subject).to_not be_valid
-  end
-  it "is not valid without a start date" do
-    subject.start_date = nil
-    expect(subject).to_not be_valid
-  end
-  it "is not valid without a duration" do
-    subject.duration = nil
-    expect(subject).to_not be_valid
+
+  describe "Validations" do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+    it "is not valid without a start time" do
+      subject.start_time = nil
+      expect(subject).to_not be_valid
+    end
+    it "is not valid without a start date" do
+      subject.start_date = nil
+      expect(subject).to_not be_valid
+    end
+    it "is not valid without a duration" do
+      subject.duration = nil
+      expect(subject).to_not be_valid
+    end
   end
 end
--- spec/models/stylist_spec.rb
+++ spec/models/stylist_spec.rb
@@ -4,19 +4,21 @@ RSpec.describe Stylist, type: :model do
   subject {
     described_class.new(first_name: "Anything", last_name: "Anything", email: "Anything")}
 
-  it "is valid with valid attributes" do
-    expect(subject).to be_valid
-  end
-  it "is not valid without a first name" do
-    subject.first_name = nil
-    expect(subject).to_not be_valid
-  end
-  it "is not valid without a last name" do
-    subject.last_name = nil
-    expect(subject).to_not be_valid
-  end
-  it "is not valid without an email" do 
-    subject.email = nil
-    expect(subject).to_not be_valid
+  describe "Validations" do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+    it "is not valid without a first name" do
+      subject.first_name = nil
+      expect(subject).to_not be_valid
+    end
+    it "is not valid without a last name" do
+      subject.last_name = nil
+      expect(subject).to_not be_valid
+    end
+    it "is not valid without an email" do 
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
   end
 end
