16b9c8f5ad72f81261303219e5123ecbc7eb40b8
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-02-26 12:11:40 -0500

    Set up application model presence of validations with unit tests

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,7 @@
 class Appointment < ApplicationRecord
-  belongs_to :stylist
-  belongs_to :client
+  # belongs_to :stylist
+  # belongs_to :client
+  validates_presence_of :start_time
+  validates_presence_of :start_date
+  validates_presence_of :duration
 end
--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,10 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Appointment, type: :model do
+  subject {
+    described_class.new(
+    start_time: "13:00:00:00", 
+    start_date: "2018-03-02",
+    duration: 120)
+  }
   it "is valid with valid attributes" do
-    expect(Appointment.new).to be_valid
+    expect(subject).to be_valid
   end
-  it "is not valid without a start time"
-  it "is not valid without a start date"
-  it "is not valid without a duration"
+  it "is not valid without a start time" do
+    subject.start_time = nil
+    expect(subject).to_not be_valid
+  end
+  it "is not valid without a start date" do
+    subject.start_date = nil
+    expect(subject).to_not be_valid
+  end
+  it "is not valid without a duration" do
+    subject.duration = nil
+    expect(subject).to_not be_valid
+  end
 end
