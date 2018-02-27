2b4f44b9d49ba4c3d911be6dd38377aa3fca4951
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-02-27 15:26:22 -0500

    Created POST route in stylists controller and confirmed through postman

--- app/controllers/api/stylists_controller.rb
+++ app/controllers/api/stylists_controller.rb
@@ -2,6 +2,23 @@ class Api::StylistsController < ApplicationController
   def show
     @stylist = Stylist.find(params[:id])
 
-    render json:@stylist
+    render json: @stylist
   end
+
+  def create
+    @stylist = Stylist.create!(stylist_params)
+
+    render json: @stylist
+  end
+
+  private
+
+  def stylist_params
+    params.require(:stylist).permit(
+      :first_name, 
+      :last_name,
+      :photo_url,
+      :email)
+  end
+
 end
