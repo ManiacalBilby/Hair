46f903d596f3bb79734ad317aa50be84b8f44e3f
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-02-27 11:26:47 -0500

    Corrected error in client model. Created and ran seeds file.

--- app/models/client.rb
+++ app/models/client.rb
@@ -5,5 +5,5 @@ class Client < ApplicationRecord
   validates_presence_of :first_name
   validates_presence_of :last_name
   validates_presence_of :phone_number
-  validates :email, presence: true, unique: true
+  validates :email, presence: true, uniqueness: true
 end
