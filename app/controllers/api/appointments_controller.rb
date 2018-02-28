fd10c613e427f575348530e65248c05f9f9cf784
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-02-28 11:39:36 -0500

    Created INDEX action in appointments controller to GET all appointments for a stylist by id and confirmed through postman

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -1,2 +1,45 @@
 class Api::AppointmentsController < ApplicationController
+
+def index
+  @appointments = Appointment.where(stylist_id: params[:stylist_id])
+
+  render json: @appointments
+end
+
+  # def show
+  #   @appointment = Appointment.find(params[:id])
+
+  #   render json: @appointment
+  # end
+
+  # def create
+  #   @appointment = Appointment.create!(appointment_params)
+
+  #   render json: @appointment
+  # end
+
+  # def update
+  #   @appointment = Appointment.find(params[:id])
+  #   @appointment.update! (appointment_params)
+
+  #   render json: @appointment
+  # end
+
+  # def destroy
+  #   @appointment = Appointment.find(params[:id])
+  #   @appointment.delete
+
+  #   render status: :ok
+  # end
+
+  # private
+
+  # def appointment_params
+  #   params.require(:appointment).permit(
+  #     :start_, 
+  #     :last_name,
+  #     :photo_url,
+  #     :email,
+  #     :password)
+  # end
 end
