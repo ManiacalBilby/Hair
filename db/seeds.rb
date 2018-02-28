e7d24513409c7aabde7551fd661fc198ce43728f
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-02-28 10:50:36 -0500

    Reformatted seeds file without hard coded id's to solve the issue with id sequence on POST

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,125 +13,85 @@ ActiveRecord::Base.connection.tables.each do |t|
   ActiveRecord::Base.connection.reset_pk_sequence!(t)
 end
 
-stylists = []
-clients = []
-appointments = []
-
-
-
-
-# amanda = Stylist.create(
-#   first_name: "Amanda",
-#   last_name: "McDonald",
-#   photo_url: "http://thecollectiveasalon.com/wp-content/uploads/2014/10/amanda.jpg",
-#   email: "alwestmcdonald@yahoo.com",
-#   password: "password",
-# )
-
-# doris = Client.create(
-#   first_name: "Doris",
-#   last_name: "Green",
-#   phone_number: "770-123-4567",
-#   formula: "Base/Redken Shades EQ equal parts 05C and 04NB; Midshaft and Ends/06CB and Dimensional Highlights/Redken Chromatics 10GI 20 volume.",
-#   notes: "Weekly shampoo and blowout",
-#   photo_url: "http://onehair.com.au/wp-content/uploads/2017/12/IMG_20171201_105520-e1513925227792.jpg",
-#   email: "jenna424@hotmail.com",
-# )
-
-# amanda.appointments.create(
-#   start_time: "15:00:00",
-#   start_date: "2018-03-01",
-#   duration: 60,
-#   comments: "Just shampoo and blowout.",
-#   client_id: 1
-# )
-
-
-
-stylists << Stylist.create(
+amanda = Stylist.create(
   first_name: "Amanda",
   last_name: "McDonald",
   photo_url: "http://thecollectiveasalon.com/wp-content/uploads/2014/10/amanda.jpg",
   email: "alwestmcdonald@yahoo.com",
-  password: "password",
-  id: 1
+  password: "password"
 )
 
-stylists << Stylist.create(
+jennifer = Stylist.create(
   first_name: "Jennifer",
   last_name: "Stills",
   photo_url: "https://static.boredpanda.com/blog/wp-content/uploads/2015/12/honest-selfie-hairstylist-ursula-goff-9.jpg",
   email: "jenniferstills@gmail.com",
   password: "password",
-  id: 2
 )
 
-clients << Client.create(
+doris = Client.create(
   first_name: "Doris",
   last_name: "Green",
   phone_number: "770-123-4567",
   formula: "Base/Redken Shades EQ equal parts 05C and 04NB; Midshaft and Ends/06CB and Dimensional Highlights/Redken Chromatics 10GI 20 volume.",
   notes: "Weekly shampoo and blowout",
   photo_url: "http://onehair.com.au/wp-content/uploads/2017/12/IMG_20171201_105520-e1513925227792.jpg",
-  email: "jenna424@hotmail.com",
-  id: 1
+  email: "jenna424@hotmail.com"
 )
 
-clients << Client.create(
+samantha = Client.create(
   first_name: "Samantha",
   last_name: "James",
   phone_number: "770-321-7654",
   formula: "Goldwell Oxycur Platin Dust Free Bleach + 30-volume developer",
   notes: "Platinum blonde every 3 weeks",
   photo_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-3JIphBePTCFVuS0HlDr5R7f8RI5g_HTtbwbSMwjekChk0XNo",
-  email: "forgivenessha@yahoo.com",
-  id: 2
+  email: "forgivenessha@yahoo.com"
 )
 
-clients << Client.create(
+karen = Client.create(
   first_name: "Karen",
   last_name: "Post",
   phone_number: "770-231=6745",
   formula: "Formula 1.- Natural Level 6- 6RC 1 ½ oz. 7CR 1 oz. 6BC ½ oz orange concentrate ¼ oz red concentrate ¼ oz gold concentrate ¼ oz 30 volume crème developer",
   notes: "Frequent color changer",
   photo_url: "https://s-media-cache-ak0.pinimg.com/originals/8e/1c/93/8e1c93fda5f87cbe0d3b1924706f53cb.jpg",
-  email: "karenpost@yahoo.com",
-  id: 3
+  email: "karenpost@yahoo.com"
 )
 
-appointments << Appointment.create(
+Appointment.create(
+  stylist: amanda, 
+  client: doris, 
   start_time: "15:00:00",
   start_date: "2018-03-01",
   duration: 60,
-  comments: "Just shampoo and blowout.",
-  stylist_id: 1,
-  client_id: 1
+  comments: "Just shampoo and blowout."
 )
 
-appointments << Appointment.create(
-  start_time: "19:00:00",
-  start_date: "2018-03-22",
-  duration: 80,
-  comments: "Trim and touch up roots",
-  stylist_id: 2,
-  client_id: 2
-)
-
-appointments << Appointment.create(
+Appointment.create(
+  stylist: amanda, 
+  client: doris, 
   start_time: "15:00:00",
   start_date: "2018-03-08",
   duration: 120,
   comments: "Cut and color",
-  stylist_id: 1,
-  client_id: 1
 )
 
-appointments << Appointment.create(
+Appointment.create(
+  stylist: jennifer,
+  client: samantha,
+  start_time: "19:00:00",
+  start_date: "2018-03-22",
+  duration: 80,
+  comments: "Trim and touch up roots",
+)
+
+Appointment.create(
+  stylist: jennifer,
+  client: karen,
   start_time: "08:00:00",
   start_date: "2018-03-13",
   duration: 120,
-  comments: "Cut and color(red)",
-  stylist_id: 2,
-  client_id: 3
+  comments: "Cut and color(red)"
 )
 
