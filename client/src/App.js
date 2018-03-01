44b2535b1db9feb70e59e6f366e15ec21e8a43ad
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-03-01 15:37:46 -0500

    Added list of appointments to ShowStylistPage

--- client/src/App.js
+++ client/src/App.js
@@ -13,29 +13,13 @@ class App extends Component {
 
   state = {
     stylists: [],
-    appointments: []
   }
 
-  componentWillMount() {
+  componentDidMount() {
     this.fetchStylists()
-    this.fetchAppointments()
   }
 
   fetchStylists = async () => {
-    try {
-      const response = await axios.get('/api/stylists')
-      console.log("Api call should return all stylists", response.data)
-      await this.setState({ stylists: response.data })
-      return response.data
-    }
-    catch (error) {
-      console.log(error)
-      await this.setState({ error: error.message })
-      return error.message
-    }
-  }
-
-  fetchAppointments = async () => {
     try {
       const response = await axios.get('/api/stylists')
       console.log("Api call should return all stylists", response.data)
--- client/src/components/ShowStylistPage.js
+++ client/src/components/ShowStylistPage.js
@@ -1,39 +1,69 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import {Link} from 'react-router-dom'
 
 class ShowStylistPage extends Component {
 
   state = {
-    stylist: {}
+    stylist: {},
+    appointments: []
   }
 
-  componentWillMount = () => {
-    this.getStylist()
+  componentWillMount = async () => {
+    const stylist = await this.getStylist()
+
+    if (!!stylist) {
+      const appointments = await this.fetchAppointments()
+
+      await this.setState({ appointments, stylist })
+    }
+
   }
 
   getStylist = () => {
 
     const params_id = this.props.match.params.id
 
-    console.log(this.props.stylists)
+    console.log("Confirm receiving stylists", this.props.stylists)
     const stylist = this.props.stylists.find((sty) => {
 
-      return sty.id === parseInt(params_id)
+      return sty.id === parseInt(params_id, 10)
     })
-    console.log(stylist)
+    console.log("Current stylist", stylist)
 
-    if (!!stylist) {
-      this.setState({ stylist })
+    return stylist;
+  }
+
+  fetchAppointments = async () => {
+    try {
+      const response = await axios.get(`/api/stylists/${this.props.match.params.id}/appointments`)
+      console.log("Api call should return all appts", response.data)
+      return response.data
+    }
+    catch (error) {
+      console.log(error)
+      await this.setState({ error: error.message })
+      return error.message
     }
   }
 
   render() {
-    console.log(this.state.stylist)
+    if (this.state.error) {
+      return <div>{this.state.error}</div>
+    }
+    console.log("Stylist in state", this.state.stylist)
     return (
       <div>
         <h1>{this.state.stylist.first_name}</h1>
-        {/* {this.state.stylist.appointments.map(appointment =>(
-          <div>{appointment.duration}</div>
-        ))} */}
+        {this.state.appointments.map(appointment => (
+          <Link key = {appointment.id} to = {`/stylists/${appointment.stylist_id}/appointments/${appointment.id}`}>
+          <div key={appointment.id}>
+            <div>{appointment.duration}</div>
+            <div>{(appointment.start_date)}</div>
+            <div>{appointment.start_time}</div>
+          </div>
+          </Link>
+        ))}
       </div>
     );
   }
