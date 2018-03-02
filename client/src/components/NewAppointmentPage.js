23604d209887a78a140ac03987d2cb33e93d704f
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-03-02 08:49:34 -0500

    Created basic NewAppointmentPage component

---
+++ client/src/components/NewAppointmentPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewAppointmentPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>New appointment page!</h1>
+      </div>
+    );
+  }
+}
+
+export default NewAppointmentPage;
