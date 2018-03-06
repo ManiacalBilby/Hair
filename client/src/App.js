7e157990b3557607a6b92c60c9ddb21cace5d683
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-03-06 11:58:28 -0500

    Created new ShowStylistPage component. Added clickable images to direct to appointments and clients pages. Link set for appointments only.

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import axios from 'axios'
 // import './App.css';
 import SplashPage from './components/SplashPage'
 import StylistsPage from './components/StylistsPage'
+import ShowStylistPage from './components/ShowStylistPage'
 import StylistAppointmentsPage from './components/StylistAppointmentsPage'
 import EditAppointmentPage from './components/EditAppointmentPage'
 import NewAppointmentPage from './components/NewAppointmentPage'
@@ -40,9 +41,11 @@ class App extends Component {
       return <div>{this.state.error}</div>
     }
     const StylistsPageComponent = () => (<StylistsPage stylists = {this.state.stylists} />)
+    const ShowStylistPageComponent = (props) => (<ShowStylistPage {...props}/>)
     const StylistAppointmentsPageComponent = (props) => (<StylistAppointmentsPage {...props} stylists = {this.state.stylists} />)
     const EditAppointmentpageComponent = (props) => (<EditAppointmentPage {...props}/>)
     const NewAppointmentPageComponent = (props) => (<NewAppointmentPage {...props}/>)
+
     
     return (
       <Router>
@@ -50,6 +53,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={SplashPage} />
             <Route exact path="/stylists" component={StylistsPageComponent} />
+            <Route exact path="/stylists/:id/" component={ShowStylistPageComponent}/>
             <Route exact path="/stylists/:id/appointments" component={StylistAppointmentsPageComponent} />
             <Route exact path="/stylists/:stylist_id/appointments/new" component={NewAppointmentPageComponent} />
             <Route exact path="/stylists/:stylist_id/appointments/:id" component={EditAppointmentpageComponent} />
---
+++ client/src/components/ShowStylistPage.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+
+const Wrapper = styled.div`
+height: 100vh;
+width: 100vw;
+display: flex;
+align-items: center;
+justify-content: space-around;
+align-content: center;
+flex-wrap: wrap;
+`
+
+const AppointmentContainer = styled.div`
+  background-image: url('https://i.imgur.com/7u5xGws.jpg?1');
+  height: 200px;
+  width: 300px;
+  margin: 15px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-size: cover;
+  background-position: center;
+  &:hover {
+    filter: blur(1px);
+  };
+  /* background-attachment: fixed; */
+  @media (min-width: 768px) {
+    height: 300px;
+    width: 500px;
+  };
+  @media (min-width: 1300px) {
+    height: 400px;
+    width: 600px;
+  };
+`
+const ClientContainer = AppointmentContainer.extend`
+background-image: url('https://i.imgur.com/hoKEA4v.jpg?2');
+`
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center; 
+  align-items: center;
+  color: gray;
+  font-size: 2rem;
+`
+
+const StyledLink = styled(Link)`
+text-decoration: none;
+`
+
+class ShowStylistPage extends Component {
+  render() {
+    return (
+      <Wrapper>
+        <StyledLink to={`/stylists/${this.props.match.params.id}/appointments`}>
+          <AppointmentContainer>
+            <Content>
+              Appointments
+          </Content>
+          </AppointmentContainer>
+        </StyledLink>
+        <ClientContainer>
+          <Content>
+            Clients
+          </Content>
+        </ClientContainer>
+      </Wrapper>
+    );
+  }
+}
+
+export default ShowStylistPage;
