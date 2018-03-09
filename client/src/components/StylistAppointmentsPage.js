ec86e9043b890d0b2190b594ab34ebfc19ce81a0
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-03-08 19:14:20 -0500

    Mobile styling for StylistAppointmentsPage. Needs media queries

--- client/src/components/StylistAppointmentsPage.js
+++ client/src/components/StylistAppointmentsPage.js
@@ -2,6 +2,41 @@ import React, { Component } from 'react';
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 import moment from 'moment'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  align-content: center;
+  flex-wrap: wrap;
+  background-color: whitesmoke;
+`
+
+const AllAppointmentsContainer = styled.div`
+  display: flex;
+  /* flex-direction: column; */
+  align-items: center;
+  justify-content: center;
+  align-content: center;
+  /* flex-wrap: wrap; */
+  width: 75%;
+`
+const StyledLink = styled(Link)`
+  display: flex;
+  justify-content: center;
+  text-decoration: none;
+  width: 100%;
+`
+
+const AppointmentContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-around;
+`
 
 class StylistAppointmentsPage extends Component {
 
@@ -75,23 +110,23 @@ render() {
   console.log("Stylist in state", this.state.stylist)
   console.log("Appointments in state", this.state.appointments)
   return (
-    <div>
-      <h1>{this.state.stylist.first_name}</h1>
+    <Wrapper>
+      <h1>{`${this.state.stylist.first_name}'s Appointments`}</h1>
 
       {this.state.appointments.map(appointment => (
-        <div key={appointment.id}>
-          <Link to={`/stylists/${appointment.stylist_id}/appointments/${appointment.id}`}>
-            <div key={appointment.id}>
-              <div>{appointment.duration}</div>
-              <div>{moment(appointment.start_date).format('DD MMM YYYY')}</div>
+        <AllAppointmentsContainer key={appointment.id}>
+          <StyledLink to={`/stylists/${appointment.stylist_id}/appointments/${appointment.id}`}>
+            <AppointmentContainer key={appointment.id}>
+              <div>{`${appointment.duration} min.`}</div>
+              <div>{moment(appointment.start_date).format('MMM DD YYYY')}</div>
               <div>{moment(appointment.start_time).format('hh:mm a')}</div>
-            </div>
-          </Link>
+            </AppointmentContainer>
+          </StyledLink>
           <button onClick={() => this.deleteAppointment(appointment.id)}>Delete</button>
-        </div>
+        </AllAppointmentsContainer>
       ))}
       <Link to={`/stylists/${this.state.stylist.id}/appointments/new`}>New Appointment</Link>
-    </div>
+    </Wrapper>
   );
 }
 }
