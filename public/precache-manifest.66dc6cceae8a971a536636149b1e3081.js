f23dd2def5a6ccb64e3d39e431370cf89334ff8d
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-11-15 17:28:14 -0500

    Updated react-scripts. Correcteded issue with eslint version

---
+++ public/precache-manifest.66dc6cceae8a971a536636149b1e3081.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "78d550fae6392f1a8cbb",
+    "url": "/static/js/main.78d550fa.chunk.js"
+  },
+  {
+    "revision": "66c8532a6eb0d0d38a1f",
+    "url": "/static/js/1.66c8532a.chunk.js"
+  },
+  {
+    "revision": "78d550fae6392f1a8cbb",
+    "url": "/static/css/main.8ac52c3e.chunk.css"
+  },
+  {
+    "revision": "66c8532a6eb0d0d38a1f",
+    "url": "/static/css/1.17d53b1d.chunk.css"
+  },
+  {
+    "revision": "65c0dec2b538c695208a982158498cb7",
+    "url": "/index.html"
+  }
+];
